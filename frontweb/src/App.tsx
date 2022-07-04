22f3ac2d506d00034a621e3a5709c97fe74d97d1
Mario Miranda <mario_chan15@hotmail.com>
2022-07-04 14:08:12 -0400

    Estrutura básica do projeto

---
+++ frontweb/src/App.tsx
@@ -0,0 +1,13 @@
+import './assets/styles/custom.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
