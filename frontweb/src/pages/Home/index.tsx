7993626abb1bd4ccbf08fec4f1c5ecbc6acc8d84
Mario Miranda <mario_chan15@hotmail.com>
2022-07-06 13:12:17 -0400

    Home responsivo

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,32 +1,21 @@
 import './styles.css';
-import { ReactComponent as CarMainImage } from 'assets/images/car-home.svg';
+import CarMainImage from 'assets/images/main-car-image.png';
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="base-card home-top-card">
-        <div className="row">
-          <div className="col-xl-6">
-            <CarMainImage />
-          </div>
-          <div className="col-xl-6">
-            <h2>Lorem ipsum dolor sit.</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia,
-              maxime!
-            </p>
-          </div>
+        <img src={CarMainImage} alt="Imagem do carro na tela principal" />
+        <div className="home-top-card-text">
+          <h2>O carro perfeito para você</h2>
+          <p>
+          Conheça nossos carros e dê mais um passo na realização do seu sonho
+          </p>
         </div>
       </div>
       <div className="base-card home-bottom-card">
-        <div className="row">
-          <div className="col-xl-6">
-            <button className="btn btn-primary">Ver catlálogo</button>
-          </div>
-          <div className="col-xl-6">
-            <p>Comece agora a navegar</p>
-          </div>
-        </div>
+        <button className="btn btn-primary">Ver catálogo</button>
+        <p>Comece agora a navegar</p>
       </div>
     </div>
   );
