d3fec5eb76e63aba81100ac1ce49a75edeed9f08
Mario Miranda <mario_chan15@hotmail.com>
2022-07-05 23:26:26 -0400

    Tentando montar o layout de outro jeito

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -3,15 +3,29 @@ import { ReactComponent as CarMainImage } from 'assets/images/car-home.svg';
 
 const Home = () => {
   return (
-    <div className="home-card">
-      <div className="base-card home-card-top">
-        <div className="car-image">
-          <CarMainImage />
+    <div className="home-container">
+      <div className="base-card home-top-card">
+        <div className="row">
+          <div className="col-xl-6">
+            <CarMainImage />
+          </div>
+          <div className="col-xl-6">
+            <h2>Lorem ipsum dolor sit.</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia,
+              maxime!
+            </p>
+          </div>
         </div>
       </div>
-      <div className="base-card home-card-bottom">
-        <div>
-          <p>Texto aqui</p>
+      <div className="base-card home-bottom-card">
+        <div className="row">
+          <div className="col-xl-6">
+            <button className="btn btn-primary">Ver catlálogo</button>
+          </div>
+          <div className="col-xl-6">
+            <p>Comece agora a navegar</p>
+          </div>
         </div>
       </div>
     </div>
